ca201aab3550ea95d40d3f3b5369c1e63bed1b61
Arnold <ArnoldBustamante@Gmail.com>
2020-04-29 11:44:22 -0400

    total time added

---
+++ scripts/helper.js
@@ -0,0 +1,12 @@
+class Helper {
+
+  playPauseAndUpdate (song) {
+    player.playPause(song);
+
+    const duration = player.currentlyPlaying.duration;
+    $('#time-control .total-time').text(duration);
+
+  }
+}
+
+const helper = new Helper();
